fb7cde6f2416a4bbb8b45e82bf56afafb97858ac
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2021-11-06 12:41:31 +0000

    Removed ssh configuration

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,13 +4,13 @@
 # out when the container is first created here
 
 # Set the root passwd - grep docker logs for it
-ROOT_PASSWORD=`pwgen -c -n -1 12`
-echo "root:$ROOT_PASSWORD" | chpasswd
-echo "root login password: $ROOT_PASSWORD"
+# ROOT_PASSWORD=`pwgen -c -n -1 12`
+# echo "root:$ROOT_PASSWORD" | chpasswd
+# echo "root login password: $ROOT_PASSWORD"
 
 # Also echo out the pg password written to
 # /PGPASSWORD.txt when the image was made
 # by start-postgres.sh
 
 # Launch supervisor
-supervisord -n
+# supervisord -n
